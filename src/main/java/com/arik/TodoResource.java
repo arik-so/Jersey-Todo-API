d23e4eeb41d77b5e1b3e40d4bab7c7d95beee1de
Arik Sosman <arik@sosman.eu>
2014-12-18 17:04:13 +0100

    configured MongoDB. Now should create collections (in theory)

--- src/main/java/com/arik/TodoResource.java
+++ src/main/java/com/arik/TodoResource.java
@@ -1,11 +1,18 @@
 package com.arik;
 
+import com.arik.persistency.PersistentStorage;
 import com.arik.twilio.TwilioConnector;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBObject;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import java.net.UnknownHostException;
+import java.util.Date;
 
 /**
  * Created by arik-so on 12/18/14.
@@ -14,6 +21,7 @@ import javax.ws.rs.Produces;
 public class TodoResource {
 
     @POST
+    @Produces("text/plain")
     public String createTodoItem(){
         return "Created Todo item";
     }
@@ -23,6 +31,26 @@ public class TodoResource {
     @Produces("text/plain")
     public String getTwilioInfo(){
         return "Twilio info: "+TwilioConnector.ACCOUNT_AUTH_TOKEN;
+    }
+
+    @GET
+    @Path("/dbtest")
+    @Produces("application/json")
+    public String testDB() throws UnknownHostException {
+
+        DB database = PersistentStorage.getDatabaseConnection();
+
+        DBCollection table = database.getCollection("tests");
+
+        BasicDBObject row = new BasicDBObject();
+        row.append("name", "Test Entry");
+        row.append("timestamp", new Date());
+
+        table.insert(row);
+
+        DBObject foundRow = table.findOne();
+        return foundRow.toString();
+
     }
 
 }
---
+++ src/main/java/com/arik/persistency/PersistentStorage.java
@@ -0,0 +1,27 @@
+package com.arik.persistency;
+
+import com.mongodb.*;
+
+import java.net.UnknownHostException;
+
+/**
+ * Created by arik-so on 12/18/14.
+ */
+public class PersistentStorage {
+
+    public static com.mongodb.DB getDatabaseConnection() throws UnknownHostException { // DB is so short and ambiguous
+
+        MongoClient mongoClient;
+
+        final String mongoSoupHost = System.getenv("MONGOSOUP_URL");
+        if(mongoSoupHost != null && mongoSoupHost.length() > 0){
+            mongoClient = new MongoClient(mongoSoupHost);
+        }else{
+            mongoClient = new MongoClient("localhost", 27017);
+        }
+
+        return mongoClient.getDB("todo-api-db");
+
+    }
+
+}
